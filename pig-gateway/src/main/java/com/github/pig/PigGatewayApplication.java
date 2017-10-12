2d2612ec0eb556633504a6831368e6b51f92ac2e
wangiegie@gmail.com <wangiegie@gmail.com>
2017-10-12 20:11:02 +0800

    [edit]  modify find-bugs find bug
    Signed-off-by: wangiegie@gmail.com <wangiegie@gmail.com>

--- pig-gateway/src/main/java/com/github/pig/PigGatewayApplication.java
+++ pig-gateway/src/main/java/com/github/pig/PigGatewayApplication.java
@@ -3,7 +3,6 @@ package com.github.pig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
