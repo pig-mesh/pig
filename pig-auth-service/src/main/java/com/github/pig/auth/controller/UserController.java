21820464a093493af793f228ca8208cb8bbf2ab4
wangiegie@gmail.com <wangiegie@gmail.com>
2017-11-11 17:49:54 +0800

    end: 选择退出后，清除redisTokenStore相关token

--- pig-auth-service/src/main/java/com/github/pig/auth/controller/UserController.java
+++ pig-auth-service/src/main/java/com/github/pig/auth/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.github.pig.auth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +15,27 @@ import java.security.Principal;
  */
 @RestController
 public class UserController {
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
     @RequestMapping("/user")
     public Object user(Principal user) {
         return user;
+    }
+
+    /**
+     * 清除Redis中 accesstoken refreshtoken
+     *
+     * @param accesstoken  accesstoken
+     * @param refreshToken refreshToken
+     * @return true/false
+     */
+    @PostMapping("/removeToken")
+    public Boolean removeToken(String accesstoken, String refreshToken) {
+        RedisTokenStore tokenStore = new RedisTokenStore(redisConnectionFactory);
+        tokenStore.removeRefreshToken(refreshToken);
+        tokenStore.removeAccessToken(accesstoken);
+        return Boolean.TRUE;
     }
 
 }
