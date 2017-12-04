a070f361cae3769db7b39c7b67f8a17fe12a5548
wangiegie@gmail.com <wangiegie@gmail.com>
2017-12-04 22:36:24 +0800

    去掉非必须校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -25,20 +25,12 @@
 </template>
 
 <script>
-import { isvalidUsername } from '@/utils/validate'
 import socialSign from './socialsignin'
 
 export default {
   components: { socialSign },
   name: 'login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!isvalidUsername(value)) {
-        callback(new Error('请输入正确的用户名'))
-      } else {
-        callback()
-      }
-    }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
         callback(new Error('密码不能小于6位'))
@@ -52,7 +44,7 @@ export default {
         password: '123456'
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        username: [{ required: true, trigger: 'blur' }],
         password: [{ required: true, trigger: 'blur', validator: validatePassword }]
       },
       pwdType: 'password',
