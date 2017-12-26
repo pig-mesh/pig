8d861589368aae49bc8a02cbf4e32d2212365200
wangiegie@gmail.com <wangiegie@gmail.com>
2017-12-26 16:36:40 +0800

    turbine 监控

--- pig-monitor/src/main/java/com/github/pig/monitor/PigMonitorApplication.java
+++ pig-monitor/src/main/java/com/github/pig/monitor/PigMonitorApplication.java
@@ -4,6 +4,7 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author lengleng
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * 监控模块
  */
 @EnableAdminServer
+@EnableTurbine
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PigMonitorApplication {
