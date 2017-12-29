db017e1941208a3ed701774c8bc1af18ee8e8603
wangiegie@gmail.com <wangiegie@gmail.com>
2017-12-29 13:05:34 +0800

    1. zipkin-elk 服务链路追踪

---
+++ pig-visual/pig-zipkin-elk/src/main/java/com/github/pig/zipkin/PigZipkinElkApplication.java
@@ -0,0 +1,19 @@
+package com.github.pig.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lengleng
+ * @date 2017-12-29 13:02:29
+ * zipkin 链路追踪
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PigZipkinElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigZipkinElkApplication.class, args);
+    }
+}
