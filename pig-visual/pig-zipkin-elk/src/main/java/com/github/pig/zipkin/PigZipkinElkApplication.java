d6dfc08a407acc64dab09fb39109bd851686ac6f
wangiegie@gmail.com <wangiegie@gmail.com>
2017-12-31 11:09:22 +0800

    整合zipkin 未整合elk

--- pig-visual/pig-zipkin-elk/src/main/java/com/github/pig/zipkin/PigZipkinElkApplication.java
+++ pig-visual/pig-zipkin-elk/src/main/java/com/github/pig/zipkin/PigZipkinElkApplication.java
@@ -3,6 +3,7 @@ package com.github.pig.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
 
 /**
  * @author lengleng
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableZipkinServer
 public class PigZipkinElkApplication {
 
     public static void main(String[] args) {
