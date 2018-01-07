69da9ae0b0391315ce9a7b9c7db4fe3d4ef75dad
wangiegie@gmail.com <wangiegie@gmail.com>
2018-01-07 15:45:57 +0800

    update： 升级以后fallback 注册成为spring bean

--- pig-auth-service/src/main/java/com/github/pig/auth/feign/fallback/UserServiceFallbackImpl.java
+++ pig-auth-service/src/main/java/com/github/pig/auth/feign/fallback/UserServiceFallbackImpl.java
@@ -4,12 +4,14 @@ import com.github.pig.auth.feign.UserService;
 import com.github.pig.common.vo.UserVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /**
  * @author lengleng
  * @date 2017/10/31
  * 用户服务的fallback
  */
+@Service
 public class UserServiceFallbackImpl implements UserService {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
--- pig-gateway/src/main/java/com/github/pig/gateway/feign/fallback/MenuServiceFallbackImpl.java
+++ pig-gateway/src/main/java/com/github/pig/gateway/feign/fallback/MenuServiceFallbackImpl.java
@@ -4,13 +4,17 @@ import com.github.pig.common.vo.MenuVo;
 import com.github.pig.gateway.feign.MenuService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 /**
  * @author lengleng
  * @date 2017/10/31
+ * why add @Service when i up version ?
+ * https://github.com/spring-cloud/spring-cloud-netflix/issues/762
  */
+@Service
 public class MenuServiceFallbackImpl implements MenuService {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Override
