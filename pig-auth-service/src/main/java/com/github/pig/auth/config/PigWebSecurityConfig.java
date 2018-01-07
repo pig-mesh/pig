99c4a9eb458f8be10ccdc76eb85d2f19dfd91a2b
wangiegie@gmail.com <wangiegie@gmail.com>
2018-01-07 15:49:34 +0800

    fixed ： 升级后 access define 问题

--- pig-auth-service/src/main/java/com/github/pig/auth/config/PigWebSecurityConfig.java
+++ pig-auth-service/src/main/java/com/github/pig/auth/config/PigWebSecurityConfig.java
@@ -1,25 +1,23 @@
 package com.github.pig.auth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @author lengleng
  * @date 2018/1/6
  * AUTH-SERVER 安全配置
- * 要在 auth-server 配置之后
+ *  why add order?
+ * https://stackoverflow.com/questions/42822875/springboot-1-5-x-security-oauth2
  */
-@Order(value = Integer.MAX_VALUE)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @Configuration
 @EnableWebSecurity
 public class PigWebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
