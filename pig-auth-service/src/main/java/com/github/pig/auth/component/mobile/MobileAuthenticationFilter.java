affb18207e58d157feb3c9b0d2222381c81c2103
wangiegie@gmail.com <wangiegie@gmail.com>
2018-01-14 20:38:42 +0800

    手机号登录调整

---
+++ pig-auth-service/src/main/java/com/github/pig/auth/component/mobile/MobileAuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.github.pig.auth.component.mobile;
+
+import com.github.pig.common.constant.SecurityConstants;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author lengleng
+ * @date 2018/1/9
+ * 手机号登录验证filter
+ */
+public class MobileAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    public static final String SPRING_SECURITY_FORM_MOBILE_KEY = "mobile";
+
+    private String mobileParameter = SPRING_SECURITY_FORM_MOBILE_KEY;
+    private boolean postOnly = true;
+
+    public MobileAuthenticationFilter() {
+        super(new AntPathRequestMatcher(SecurityConstants.MOBILE_TOKEN_URL, "POST"));
+    }
+
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        if (postOnly && !request.getMethod().equals(HttpMethod.POST.name())) {
+            throw new AuthenticationServiceException(
+                    "Authentication method not supported: " + request.getMethod());
+        }
+
+        String mobile = obtainMobile(request);
+
+        if (mobile == null) {
+            mobile = "";
+        }
+
+        mobile = mobile.trim();
+
+        MobileAuthenticationToken mobileAuthenticationToken = new MobileAuthenticationToken(mobile);
+
+        setDetails(request, mobileAuthenticationToken);
+
+        return this.getAuthenticationManager().authenticate(mobileAuthenticationToken);
+    }
+
+    protected String obtainMobile(HttpServletRequest request) {
+        return request.getParameter(mobileParameter);
+    }
+
+    protected void setDetails(HttpServletRequest request,
+                              MobileAuthenticationToken authRequest) {
+        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+    }
+
+    public void setPostOnly(boolean postOnly) {
+        this.postOnly = postOnly;
+    }
+
+    public String getMobileParameter() {
+        return mobileParameter;
+    }
+
+    public void setMobileParameter(String mobileParameter) {
+        this.mobileParameter = mobileParameter;
+    }
+
+    public boolean isPostOnly() {
+        return postOnly;
+    }
+}
+
