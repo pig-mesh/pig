30501ae2e1be3356b80ccc431acf4327f35c77d4
wangiegie@gmail.com <wangiegie@gmail.com>
2018-01-23 13:45:40 +0800

    fixed： swagger文档不能被iframe嵌套问题

--- pig-gateway/src/main/java/com/github/pig/gateway/config/ResourceServerConfiguration.java
+++ pig-gateway/src/main/java/com/github/pig/gateway/config/ResourceServerConfiguration.java
@@ -36,6 +36,8 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class);
+        //允许使用iframe 嵌套，避免swagger-ui 不被加载的问题
+        http.headers().frameOptions().disable();
         ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = http
                 .authorizeRequests();
         for (String url : filterUrlsPropertiesConifg.getAnon()) {
