5029b93d3a674480a10b3921c9061db72e4acdca
wangiegie@gmail.com <wangiegie@gmail.com>
2018-01-23 14:36:12 +0800

    监控

---
+++ src/views/admin/monitor/cache.vue
@@ -0,0 +1,12 @@
+<template>
+    <iframe src="http://localhost:5005/manage/total/list.do" frameborder="0" style="border:0;" width="100%" height="600"></iframe>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/admin/monitor/swagger.vue
@@ -0,0 +1,12 @@
+<template>
+    <iframe src="http://localhost:9999/swagger-ui.html" frameborder="0" style="border:0;" width="100%" height="600"></iframe>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+
+</style>
