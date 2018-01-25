777a0ff3ef88902707e6e3a0c5bfe6cf753f6341
wangiegie@gmail.com <wangiegie@gmail.com>
2018-01-25 11:20:57 +0800

    pinpoint监控

---
+++ src/views/admin/monitor/pinpoint.vue
@@ -0,0 +1,12 @@
+<template>
+    <iframe src="http://106.14.69.75:28080" frameborder="0" style="border:0;" width="100%" height="600"></iframe>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+
+</style>
