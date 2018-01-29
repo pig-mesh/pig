24945fc74089af822e097c44670be391c2bfc0b2
wangiegie@gmail.com <wangiegie@gmail.com>
2018-01-29 21:50:41 +0800

    iframe 高度自适配

--- src/views/admin/monitor/zipkin.vue
+++ src/views/admin/monitor/zipkin.vue
@@ -1,9 +1,19 @@
 <template>
-    <iframe src="http://localhost:5002" frameborder="0" style="border:0;" width="100%" height="800"></iframe>
+    <iframe ref="iframe" src="http://localhost:5002" frameborder="0" style="border:0;" width="100%" height="800"></iframe>
 </template>
 
 <script>
   export default {
+    mounted() {
+      this.iframeInit()
+    },
+    methods: {
+      iframeInit() {
+        const iframe = this.$refs.iframe
+        const clientHeight = document.documentElement.clientHeight - 84
+        iframe.style.height = `${clientHeight}px`
+      }
+    }
   }
 </script>
 
