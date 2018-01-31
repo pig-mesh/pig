5d81b3c81495b97854b8a94b8b71bf3291d25b2d
冷冷 <wangiegie@gmail.com>
2018-01-31 23:25:29 +0800

    fixed: 无法监控SSO客户端信息  thx@昭华不负

---
+++ pig-modules/pig-sso-client-demo/src/main/java/com/github/pig/sso/config/ResourceServerConfiguration.java
@@ -0,0 +1,26 @@
+package com.github.pig.sso.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * @author lengleng
+ * @date 2018年1月31日23:15:08
+ * 当前配置 暴露监控信息
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+}
