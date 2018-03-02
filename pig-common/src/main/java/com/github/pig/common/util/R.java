53017afbb71005c5fd1453b22bed8fcdd9febe01
冷冷 <wangiegie@gmail.com>
2018-03-02 13:38:19 +0800

    优化： R返回对象信息构造方法

--- pig-common/src/main/java/com/github/pig/common/util/R.java
+++ pig-common/src/main/java/com/github/pig/common/util/R.java
@@ -35,6 +35,12 @@ public class R<T> implements Serializable {
         this.data = data;
     }
 
+    public R(T data, String msg) {
+        super();
+        this.data = data;
+        this.msg = msg;
+    }
+
     public R(Throwable e) {
         super();
         this.msg = e.getMessage();
