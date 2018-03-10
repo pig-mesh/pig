e4ba659a1c833f4672cda4113d397171ad8a5fbb
冷冷 <wangiegie@gmail.com>
2018-03-11 00:37:24 +0800

    1.重构认证中心，与资源服务器解耦 2. SSO统一认证页面。

--- pig-auth/src/main/java/com/github/pig/auth/serivce/UserDetailServiceImpl.java
+++ pig-auth/src/main/java/com/github/pig/auth/serivce/UserDetailServiceImpl.java
@@ -2,21 +2,11 @@ package com.github.pig.auth.serivce;
 
 import com.github.pig.auth.feign.UserService;
 import com.github.pig.auth.util.UserDetailsImpl;
-import com.github.pig.common.vo.SysRole;
 import com.github.pig.common.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.social.security.SocialUser;
-import org.springframework.social.security.SocialUserDetails;
-import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.stereotype.Service;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author lengleng
@@ -24,7 +14,7 @@ import java.util.List;
  * <p>
  */
 @Service("userDetailService")
-public class UserDetailServiceImpl implements UserDetailsService,SocialUserDetailsService, Serializable {
+public class UserDetailServiceImpl implements UserDetailsService {
     @Autowired
     private UserService userService;
 
@@ -32,23 +22,5 @@ public class UserDetailServiceImpl implements UserDetailsService,SocialUserDetai
     public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
         UserVo userVo = userService.findUserByUsername(username);
         return new UserDetailsImpl(userVo);
-    }
-
-    /**
-     * @param userId the user ID used to lookup the user details
-     * @return the SocialUserDetails requested
-     * @see UserDetailsService#loadUserByUsername(String)
-     */
-    @Override
-    public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
-        UserVo userVo = userService.findUserByOpenId(userId);
-        if (userVo == null){
-            throw new UsernameNotFoundException("用户未绑定");
-        }
-        List<GrantedAuthority> authorityList = new ArrayList<>();
-        for (SysRole role : userVo.getRoleList()) {
-            authorityList.add(new SimpleGrantedAuthority(role.getRoleCode()));
-        }
-        return new SocialUser(userVo.getUsername(),userVo.getPassword(), authorityList);
     }
 }
