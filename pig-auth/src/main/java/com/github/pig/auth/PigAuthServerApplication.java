373d141314c04db13e9784444d9ef545d72ddae1
冷冷 <wangiegie@gmail.com>
2018-03-15 09:48:11 +0800

    fixed 去掉无用注解

--- pig-auth/src/main/java/com/github/pig/auth/PigAuthServerApplication.java
+++ pig-auth/src/main/java/com/github/pig/auth/PigAuthServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author lengleng
@@ -14,7 +13,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  *         EnableResourceServer
  */
 @SpringBootApplication
-@EnableResourceServer
 @EnableDiscoveryClient
 @EnableFeignClients
 @ComponentScan(basePackages = {"com.github.pig.auth", "com.github.pig.common.bean"})
