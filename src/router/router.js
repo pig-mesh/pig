e8fffa38a90321aaa8f9c03bbff274ae3e967440
smallwei <1634566606@qq.com>
2018-04-23 11:26:05 +0800

    iframe keepAlive 为false

--- src/router/router.js
+++ src/router/router.js
@@ -29,18 +29,19 @@ export const asyncRouterMap = [
 	{ path: '/login', name: '登录页', component: _import('login/index') },
 	{ path: '/lock', name: '锁屏页', component: _import('lock/index') },
 	{ path: '*', redirect: '/404', hidden: true },
-	{ path: '/404', component: _import('error-page/404','components'), name: '404' },
-	{ path: '/403', component: _import('error-page/403','components'), name: '403' },
-	{ path: '/500', component: _import('error-page/500','components'), name: '500' },
+	{ path: '/404', component: _import('error-page/404', 'components'), name: '404' },
+	{ path: '/403', component: _import('error-page/403', 'components'), name: '403' },
+	{ path: '/500', component: _import('error-page/500', 'components'), name: '500' },
 	{
 		path: '/myiframe',
 		component: Layout,
 		redirect: '/myiframe',
+		meta: { keepAlive: true },
 		children: [
 			{
 				path: ":routerPath",
 				name: 'iframe',
-				component: _import('iframe/main','components'),
+				component: _import('iframe/main', 'components'),
 				props: true
 			}
 		]
