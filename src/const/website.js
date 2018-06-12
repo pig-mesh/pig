f33345bf03218c7382e437435437375f729fe825
冷冷 <wangiegie@gmail.com>
2018-06-13 07:57:29 +0800

    首页新增系统版本号

--- src/const/website.js
+++ src/const/website.js
@@ -26,7 +26,8 @@ export default {
       '是一个基于Spring Cloud、oAuth2.0开发基于Vue前后分离的开发平台',
       ' 是一个基于vue+vuex+vue-router快速后台管理系统，采用token交互验证方式。',
       '最大程度上帮助企业节省时间成本和费用开支。 ',
-      'QQ群：23754102'
+      'QQ群：23754102',
+      '当前版本：v1.1.2'
     ]
   },
   wel: {
@@ -35,7 +36,8 @@ export default {
       '是一个基于Spring Cloud、oAuth2.0开发基于Vue前后分离的开发平台',
       ' 是一个基于vue+vuex+vue-router快速后台管理系统，采用token交互验证方式。',
       '最大程度上帮助企业节省时间成本和费用开支。 ',
-      'QQ群：23754102'
+      'QQ群：23754102',
+      '当前版本：v1.1.2'
     ]
   }
 }
