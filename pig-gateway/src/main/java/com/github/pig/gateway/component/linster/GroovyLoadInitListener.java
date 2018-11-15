cf5328c30b0615f8baad09f629c206ad11b0957a
冷冷 <wangiegie@gmail.com>
2018-11-15 15:33:22 +0800

    :bookmark: V1.3.1. support zuul dynamic groovy filters

---
+++ pig-gateway/src/main/java/com/github/pig/gateway/component/linster/GroovyLoadInitListener.java
@@ -0,0 +1,57 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+
+package com.github.pig.gateway.component.linster;
+
+import com.netflix.zuul.FilterFileManager;
+import com.netflix.zuul.FilterLoader;
+import com.netflix.zuul.groovy.GroovyCompiler;
+import com.netflix.zuul.groovy.GroovyFileFilter;
+import com.netflix.zuul.monitoring.MonitoringHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lengleng
+ * @date 2018/11/15
+ * <p>
+ * 动态filter 初始化配置
+ */
+@Slf4j
+@Component
+@ConditionalOnProperty("zuul.groovy.path")
+public class GroovyLoadInitListener {
+    @Value("${zuul.groovy.path}")
+    private String groovyPath;
+
+    @EventListener(value = {EmbeddedServletContainerInitializedEvent.class})
+    public void init() {
+        MonitoringHelper.initMocks();
+        FilterLoader.getInstance().setCompiler(new GroovyCompiler());
+        FilterFileManager.setFilenameFilter(new GroovyFileFilter());
+        try {
+            FilterFileManager.init(10, groovyPath);
+        } catch (Exception e) {
+            log.error("初始化网关Groovy 文件失败 {}", e);
+        }
+        log.warn("初始化网关Groovy 文件成功");
+    }
+}
