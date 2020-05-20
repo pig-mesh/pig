ccd6db3063b47d64bf8c1d05cd0747d203da6617
lengleng <wangiegie@gmail.com>
2020-05-20 15:22:03 +0800

    :lock: copyright about ace

--- pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/dto/TreeNode.java
+++ pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/dto/TreeNode.java
@@ -1,20 +1,9 @@
 /*
  *
- *  *  Copyright (c) 2019-2020, 冷冷 (wangiegie@gmail.com).
- *  *  <p>
- *  *  Licensed under the GNU Lesser General Public License 3.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  *  You may obtain a copy of the License at
- *  *  <p>
- *  * https://www.gnu.org/licenses/lgpl.html
- *  *  <p>
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *
+ * 此类来自 https://gitee.com/geek_qi/cloud-platform/blob/master/ace-common/src/main/java/com/github/wxiaoqi/security/common/vo/TreeNode.java
+ * @ Apache-2.0
  */
+
 
 package com.pig4cloud.pig.admin.api.dto;
 
@@ -24,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * @author ace
  * @author lengleng
  * @date 2017年11月9日23:33:45
  */
--- pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/vo/TreeUtil.java
+++ pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/vo/TreeUtil.java
@@ -1,19 +1,7 @@
 /*
  *
- *  *  Copyright (c) 2019-2020, 冷冷 (wangiegie@gmail.com).
- *  *  <p>
- *  *  Licensed under the GNU Lesser General Public License 3.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  *  You may obtain a copy of the License at
- *  *  <p>
- *  * https://www.gnu.org/licenses/lgpl.html
- *  *  <p>
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- *
+ * 此类来自 https://gitee.com/geek_qi/cloud-platform/blob/master/ace-common/src/main/java/com/github/wxiaoqi/security/common/util/TreeUtil.java
+ * @ Apache-2.0
  */
 
 package com.pig4cloud.pig.admin.api.vo;
@@ -28,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * @author Ace
  * @author lengleng
  * @date 2020-02-09
  */
