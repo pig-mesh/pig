92ccf3eb691c178059c823aaa7d9ffc362ad9043
lbwNB <wangiegie@163.com>
2020-10-04 19:51:12 +0800

    :sparkles: Introducing new features. 优化算数验证码

--- pig-gateway/src/main/java/com/pig4cloud/pig/gateway/handler/ImageCodeHandler.java
+++ pig-gateway/src/main/java/com/pig4cloud/pig/gateway/handler/ImageCodeHandler.java
@@ -16,9 +16,9 @@
 
 package com.pig4cloud.pig.gateway.handler;
 
+import com.pig4cloud.captcha.ArithmeticCaptcha;
 import com.pig4cloud.pig.common.core.constant.CacheConstants;
 import com.pig4cloud.pig.common.core.constant.SecurityConstants;
-import com.wf.captcha.ArithmeticCaptcha;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
