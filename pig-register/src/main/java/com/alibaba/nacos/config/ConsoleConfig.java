8fd8756c85d8bf7d829cc9bf93b756634ed2195d
415084900@qq.com <415084900@qq.com>
2021-04-13 20:26:34 +0800

    修改nacos跨域问题

--- pig-register/src/main/java/com/alibaba/nacos/config/ConsoleConfig.java
+++ pig-register/src/main/java/com/alibaba/nacos/config/ConsoleConfig.java
@@ -52,7 +52,7 @@ public class ConsoleConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
+		config.addAllowedOriginPattern("*");
 		config.addAllowedHeader("*");
 		config.setMaxAge(18000L);
 		config.addAllowedMethod("*");
