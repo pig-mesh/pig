af7eab24222ec9b03acbb4d1fc1b09d2c050c0e3
lbw <wangiegie@163.com>
2021-04-16 09:35:21 +0800

    :sparkles: 增加运行环境判断，避免nacos-server 在windows 中文路径运行环境初始化失败问题

--- pig-register/src/main/java/com/alibaba/nacos/PigNacosApplication.java
+++ pig-register/src/main/java/com/alibaba/nacos/PigNacosApplication.java
@@ -16,7 +16,10 @@
 
 package com.alibaba.nacos;
 
+import cn.hutool.core.lang.Validator;
+import cn.hutool.system.SystemUtil;
 import com.alibaba.nacos.config.ConfigConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -26,15 +29,33 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * <p>
  * nacos console 源码运行，方便开发 生产建议从官网下载最新版配置运行
  */
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class PigNacosApplication {
 
 	public static void main(String[] args) {
+		if (initEnv()) {
+			SpringApplication.run(PigNacosApplication.class, args);
+		}
+	}
+
+	/**
+	 * 初始化运行环境
+	 */
+	private static boolean initEnv() {
+		// 特殊处理 window 且用户名包含中文的用户，避免RocksDB 初始化失败
+		if (SystemUtil.getOsInfo().isWindows()
+				&& Validator.hasChinese(SystemUtil.getUserInfo().getName())) {
+			log.error("路径包含中文,需要在以下配置指定不包含中文目录");
+			System.setProperty(SystemUtil.USER_HOME, "/nacos-path/");
+			return false;
+		}
+
 		System.setProperty(ConfigConstants.STANDALONE_MODE, "true");
 		System.setProperty(ConfigConstants.AUTH_ENABLED, "false");
 		System.setProperty(ConfigConstants.LOG_BASEDIR, "logs");
-		SpringApplication.run(PigNacosApplication.class, args);
+		return true;
 	}
 
 }
