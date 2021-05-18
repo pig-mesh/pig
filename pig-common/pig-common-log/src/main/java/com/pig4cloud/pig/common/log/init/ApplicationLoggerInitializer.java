84f5e6ee37ff397476331407bb3a7aae51684e8e
如梦技术 <596392912@qq.com>
2021-05-18 17:10:54 +0800

    :bug: Fixing ApplicationLoggerInitializer use EnvironmentPostProcessor.

--- pig-common/pig-common-log/src/main/java/com/pig4cloud/pig/common/log/init/ApplicationLoggerInitializer.java
+++ pig-common/pig-common-log/src/main/java/com/pig4cloud/pig/common/log/init/ApplicationLoggerInitializer.java
@@ -16,8 +16,9 @@
 
 package com.pig4cloud.pig.common.log.init;
 
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
@@ -26,17 +27,20 @@ import org.springframework.core.env.ConfigurableEnvironment;
  * <p>
  * 通过环境变量的形式注入 logging.file 自动维护 Spring Boot Admin Logger Viewer
  */
-public class ApplicationLoggerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+public class ApplicationLoggerInitializer implements EnvironmentPostProcessor, Ordered {
 
 	@Override
-	public void initialize(ConfigurableApplicationContext applicationContext) {
-		ConfigurableEnvironment environment = applicationContext.getEnvironment();
-
+	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 		String appName = environment.getProperty("spring.application.name");
-
 		String logBase = environment.getProperty("LOGGING_PATH", "logs");
+
 		// spring boot admin 直接加载日志
 		System.setProperty("logging.file.name", String.format("%s/%s/debug.log", logBase, appName));
+	}
+
+	@Override
+	public int getOrder() {
+		return Ordered.LOWEST_PRECEDENCE;
 	}
 
 }
