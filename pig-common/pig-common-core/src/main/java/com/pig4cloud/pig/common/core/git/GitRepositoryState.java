f87eddd57eb4c76c958f44adb678ca36187ea489
lishangbu <shangshili@hotmail.com>
2021-10-26 18:02:18 +0800

    :bug: Fixing a bug. 修复一些git仓库状态端点可能存在的一些潜在的问题,向下兼容git-commit-id-maven-plugin 4.x 版本

--- pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/git/GitRepositoryState.java
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/git/GitRepositoryState.java
@@ -13,62 +13,82 @@ import java.util.Properties;
  */
 @Data
 public class GitRepositoryState implements Serializable {
-    private String tags;
-    private String branch;
-    private String dirty;
-    private String remoteOriginUrl;
 
-    private String commitId;
-    private String commitIdAbbrev;
-    private String describe;
-    private String describeShort;
-    private String commitUserName;
-    private String commitUserEmail;
-    private String commitMessageFull;
-    private String commitMessageShort;
-    private String commitTime;
-    private String closestTagName;
-    private String closestTagCommitCount;
+	private String tags;
 
-    private String buildUserName;
-    private String buildUserEmail;
-    private String buildTime;
-    private String buildHost;
-    private String buildVersion;
-    private String buildNumber;
-    private String buildNumberUnique;
+	private String branch;
 
-    private String totalCommitCount;
+	private String dirty;
 
-    public GitRepositoryState() {
-    }
+	private String remoteOriginUrl;
 
-    public GitRepositoryState(Properties properties) {
-        this.tags = properties.getProperty("git.tags", "");
-        this.branch = properties.getProperty("git.branch", "");
-        this.dirty = properties.getProperty("git.dirty", "");
-        this.remoteOriginUrl = properties.getProperty("git.remote.origin.url", "");
-        this.commitId = properties.getProperty("git.commit.id.full", "");
-        this.commitIdAbbrev = properties.getProperty("git.commit.id.abbrev", "");
-        this.describe = properties.getProperty("git.commit.id.describe", "");
-        this.describeShort = properties.getProperty("git.commit.id.describe-short", "");
-        this.commitUserName = properties.getProperty("git.commit.user.name", "");
-        this.commitUserEmail = properties.getProperty("git.commit.user.email", "");
-        this.commitMessageFull = properties.getProperty("git.commit.message.full", "");
-        this.commitMessageShort = properties.getProperty("git.commit.message.short", "");
-        this.commitTime = properties.getProperty("git.commit.time", "");
-        this.closestTagName = properties.getProperty("git.closest.tag.name", "");
-        this.closestTagCommitCount = properties.getProperty("git.closest.tag.commit.count", "");
+	private String commitId;
 
-        this.buildUserName = properties.getProperty("git.build.user.name", "");
-        this.buildUserEmail = properties.getProperty("git.build.user.email", "");
-        this.buildTime = properties.getProperty("git.build.time", "");
-        this.buildHost = properties.getProperty("git.build.host", "");
-        this.buildVersion = properties.getProperty("git.build.version", "");
-        this.buildNumber = properties.getProperty("git.build.number", "");
-        this.buildNumberUnique = properties.getProperty("git.build.number.unique", "");
+	private String commitIdAbbrev;
 
-        this.totalCommitCount = properties.getProperty("git.total.commit.count", "");
-    }
+	private String describe;
+
+	private String describeShort;
+
+	private String commitUserName;
+
+	private String commitUserEmail;
+
+	private String commitMessageFull;
+
+	private String commitMessageShort;
+
+	private String commitTime;
+
+	private String closestTagName;
+
+	private String closestTagCommitCount;
+
+	private String buildUserName;
+
+	private String buildUserEmail;
+
+	private String buildTime;
+
+	private String buildHost;
+
+	private String buildVersion;
+
+	private String buildNumber;
+
+	private String buildNumberUnique;
+
+	private String totalCommitCount;
+
+	public GitRepositoryState() {
+	}
+
+	public GitRepositoryState(Properties properties) {
+		this.tags = properties.getProperty("git.tags", "");
+		this.branch = properties.getProperty("git.branch", "");
+		this.dirty = properties.getProperty("git.dirty", "");
+		this.remoteOriginUrl = properties.getProperty("git.remote.origin.url", "");
+		this.commitId = properties.getProperty("git.commit.id", "");
+		this.commitIdAbbrev = properties.getProperty("git.commit.id.abbrev", "");
+		this.describe = properties.getProperty("git.commit.id.describe", "");
+		this.describeShort = properties.getProperty("git.commit.id.describe-short", "");
+		this.commitUserName = properties.getProperty("git.commit.user.name", "");
+		this.commitUserEmail = properties.getProperty("git.commit.user.email", "");
+		this.commitMessageFull = properties.getProperty("git.commit.message.full", "");
+		this.commitMessageShort = properties.getProperty("git.commit.message.short", "");
+		this.commitTime = properties.getProperty("git.commit.time", "");
+		this.closestTagName = properties.getProperty("git.closest.tag.name", "");
+		this.closestTagCommitCount = properties.getProperty("git.closest.tag.commit.count", "");
+
+		this.buildUserName = properties.getProperty("git.build.user.name", "");
+		this.buildUserEmail = properties.getProperty("git.build.user.email", "");
+		this.buildTime = properties.getProperty("git.build.time", "");
+		this.buildHost = properties.getProperty("git.build.host", "");
+		this.buildVersion = properties.getProperty("git.build.version", "");
+		this.buildNumber = properties.getProperty("git.build.number", "");
+		this.buildNumberUnique = properties.getProperty("git.build.number.unique", "");
+
+		this.totalCommitCount = properties.getProperty("git.total.commit.count", "");
+	}
 
 }
--- pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/git/GitRepositoryStateEndpoint.java
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/git/GitRepositoryStateEndpoint.java
@@ -1,29 +1,62 @@
 package com.pig4cloud.pig.common.core.git;
 
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
  * 版本号端点
+ * <p>
+ * 可以通过该接口检查线上版本与代码库中的版本的一致性
+ * <p>
+ * 实现发布的二进制文件和代码进行关联
+ * <p>
+ * 该功能依赖于<a href=
+ * "https://github.com/git-commit-id/git-commit-id-maven-plugin">git-commit-id-maven-plugin</a>实现
+ * <p>
+ * 因此,只适用于项目构建工具为<a href="https://maven.apache.org/">maven</a>,版本控制软件为<a href=
+ * "https://git-scm.com">git</a>的情况
  *
  * @author lishangbu
  * @date 2021/10/10
  */
+@Slf4j
 @Endpoint(id = "gitrepositorystate")
 public class GitRepositoryStateEndpoint {
 
-    private GitRepositoryState gitRepositoryState;
+	private GitRepositoryState gitRepositoryState;
 
-    @ReadOperation
-    public GitRepositoryState getGitRepositoryState() throws IOException {
-        if (gitRepositoryState == null) {
-            Properties properties = new Properties();
-            properties.load(getClass().getClassLoader().getResourceAsStream("git.properties"));
-            gitRepositoryState = new GitRepositoryState(properties);
-        }
-        return gitRepositoryState;
-    }
+	@ReadOperation
+	public GitRepositoryState getGitRepositoryState() throws IOException {
+		if (gitRepositoryState != null) {
+			return gitRepositoryState;
+		}
+		try {
+			File file = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "git.properties");
+			@Cleanup
+			InputStream inputStream = new FileInputStream(file);
+			if (inputStream != null) {
+				Properties properties = new Properties();
+				properties.load(inputStream);
+				gitRepositoryState = new GitRepositoryState(properties);
+				return gitRepositoryState;
+			}
+		}
+		catch (RuntimeException e) {
+			log.error("加载git资源文件git.properties失败,错误原因为:[{}]", e.getMessage(), e);
+		}
+		// 资源文件加载可能没有报错，但在某些情况下可能为空,此时,日志里给出提示,交给开发人员自行解决
+		log.warn("git仓库状态信息初始化失败,您将无法调用/actuator/gitrepositorystate端点");
+		log.warn("请保证这个仓库是一个git托管的仓库，并且经过了maven的编译");
+		return null;
+	}
+
 }
