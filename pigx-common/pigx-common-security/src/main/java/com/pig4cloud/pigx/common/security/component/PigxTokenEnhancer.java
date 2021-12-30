041cf3a53b7c1ca0f50a3d3e357ef9f09a0e42c6
lbw <wangiegie@163.com>
2021-12-30 21:21:40 +0800

    :bug: Fixing a bug. close #3521 【令牌管理】不显示用户名和用户ID

--- pigx-common/pigx-common-security/src/main/java/com/pig4cloud/pigx/common/security/component/PigxTokenEnhancer.java
+++ pigx-common/pigx-common-security/src/main/java/com/pig4cloud/pigx/common/security/component/PigxTokenEnhancer.java
@@ -18,6 +18,7 @@
 package com.pig4cloud.pigx.common.security.component;
 
 import com.pig4cloud.pigx.common.core.constant.SecurityConstants;
+import com.pig4cloud.pigx.common.security.service.PigxUser;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -51,6 +52,15 @@ public class PigxTokenEnhancer implements TokenEnhancer {
 		additionalInfo.put(SecurityConstants.DETAILS_LICENSE, SecurityConstants.PIGX_LICENSE);
 		additionalInfo.put(SecurityConstants.ACTIVE, Boolean.TRUE);
 
+		// 客户端模式不返回具体用户信息
+		if (SecurityConstants.CLIENT_CREDENTIALS.equals(authentication.getOAuth2Request().getGrantType())) {
+			((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+			return accessToken;
+		}
+
+		PigxUser pigxUser = (PigxUser) authentication.getUserAuthentication().getPrincipal();
+		additionalInfo.put(SecurityConstants.DETAILS_USER_ID, pigxUser.getId());
+		additionalInfo.put(SecurityConstants.DETAILS_USERNAME, pigxUser.getUsername());
 		((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
 		return accessToken;
 	}
