9a366cd78aedace20b26b5d305d31db1a3a0c49a
qraddx <eoyacm81453@unionstone.com.cn>
2022-02-18 14:59:24 +0800

    :zap: 优化， 发号器获取数据源类型支持 druid jdbc url 配置形式

--- pigx-common/pigx-common-sequence/src/main/java/com/pig4cloud/pigx/common/sequence/range/impl/db/provider/SqlProviderFactory.java
+++ pigx-common/pigx-common-sequence/src/main/java/com/pig4cloud/pigx/common/sequence/range/impl/db/provider/SqlProviderFactory.java
@@ -1,10 +1,12 @@
 package com.pig4cloud.pigx.common.sequence.range.impl.db.provider;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.toolkit.JdbcUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -20,12 +22,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SqlProviderFactory {
 
+	private final DataSourceProperties dataSourceProperties;
+
 	private final List<SqlProvider> sqlProviders;
 
-	private final DataSourceProperties dataSourceProperties;
+	private final Environment environment;
 
 	public SqlProvider getSqlProvider() {
 		String url = dataSourceProperties.getUrl();
+		// druid 形式 进行降级获取
+		if (StrUtil.isBlank(url)) {
+			url = environment.getProperty("spring.datasource.druid.url");
+		}
+
 		DbType dbType = JdbcUtils.getDbType(url);
 		for (SqlProvider sqlProvider : sqlProviders) {
 			if (sqlProvider.support(dbType)) {
