996c6fbef395b84f926da3d9d6ab481097415513
Kavya <kv919136@dal.ca>
2022-03-25 02:45:29 -0300

    Extract class

---
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/config/setOpTemplate.java
@@ -0,0 +1,18 @@
+package com.pig4cloud.pig.common.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.ZSetOperations;
+
+public class setOpTemplate {
+	@Bean
+	public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
+		return redisTemplate.opsForSet();
+	}
+
+	@Bean
+	public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
+		return redisTemplate.opsForZSet();
+	}
+}
