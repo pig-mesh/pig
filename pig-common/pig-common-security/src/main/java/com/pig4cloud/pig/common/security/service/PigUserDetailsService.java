dc42b6ed9c12bc84556170e1891dbe93e4e2ffe4
Kavya <kv919136@dal.ca>
2022-03-25 04:10:46 -0300

    Push Down

--- pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/service/PigUserDetailsService.java
+++ pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/service/PigUserDetailsService.java
@@ -1,23 +1,8 @@
 package com.pig4cloud.pig.common.security.service;
 
-import cn.hutool.core.util.ArrayUtil;
-import cn.hutool.core.util.StrUtil;
-import com.pig4cloud.pig.admin.api.dto.UserInfo;
-import com.pig4cloud.pig.admin.api.entity.SysUser;
-import com.pig4cloud.pig.common.core.constant.CommonConstants;
-import com.pig4cloud.pig.common.core.constant.SecurityConstants;
-import com.pig4cloud.pig.common.core.util.R;
 import org.springframework.core.Ordered;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author lengleng
@@ -40,37 +25,6 @@ public interface PigUserDetailsService extends UserDetailsService, Ordered {
 	 */
 	default int getOrder() {
 		return 0;
-	}
-
-	/**
-	 * 构建userdetails
-	 * @param result 用户信息
-	 * @return UserDetails
-	 */
-	default UserDetails getUserDetails(R<UserInfo> result) {
-		if (result == null || result.getData() == null) {
-			throw new UsernameNotFoundException("用户不存在");
-		}
-
-		UserInfo info = result.getData();
-		Set<String> dbAuthsSet = new HashSet<>();
-
-		if (ArrayUtil.isNotEmpty(info.getRoles())) {
-			// 获取角色
-			Arrays.stream(info.getRoles()).forEach(role -> dbAuthsSet.add(SecurityConstants.ROLE + role));
-			// 获取资源
-			dbAuthsSet.addAll(Arrays.asList(info.getPermissions()));
-
-		}
-
-		Collection<? extends GrantedAuthority> authorities = AuthorityUtils
-				.createAuthorityList(dbAuthsSet.toArray(new String[0]));
-		SysUser user = info.getSysUser();
-
-		// 构造security用户
-		return new PigUser(user.getUserId(), user.getDeptId(), user.getUsername(),
-				SecurityConstants.BCRYPT + user.getPassword(), user.getPhone(), true, true, true,
-				StrUtil.equals(user.getLockFlag(), CommonConstants.STATUS_NORMAL), authorities);
 	}
 
 	/**
