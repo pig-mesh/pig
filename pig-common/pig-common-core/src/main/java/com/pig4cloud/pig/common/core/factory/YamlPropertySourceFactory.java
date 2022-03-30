1f7d79f00681e4c46d84a5a6c63d0f59c34c3339
qraddx <eoyacm81453@unionstone.com.cn>
2022-03-30 08:46:33 +0800

    :sparkles: Introducing new features. pig-common-seata

---
+++ pig-common/pig-common-core/src/main/java/com/pig4cloud/pig/common/core/factory/YamlPropertySourceFactory.java
@@ -0,0 +1,44 @@
+package com.pig4cloud.pig.common.core.factory;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.lang.Nullable;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author lengleng
+ * @date 2022/3/29
+ *
+ * 读取自定义 yaml 文件工厂类
+ */
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+	@Override
+	public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource resource) throws IOException {
+		Properties propertiesFromYaml = loadYamlIntoProperties(resource);
+		String sourceName = name != null ? name : resource.getResource().getFilename();
+		return new PropertiesPropertySource(sourceName, propertiesFromYaml);
+	}
+
+	private Properties loadYamlIntoProperties(EncodedResource resource) throws FileNotFoundException {
+		try {
+			YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+			factory.setResources(resource.getResource());
+			factory.afterPropertiesSet();
+			return factory.getObject();
+		}
+		catch (IllegalStateException e) {
+			Throwable cause = e.getCause();
+			if (cause instanceof FileNotFoundException)
+				throw (FileNotFoundException) e.getCause();
+			throw e;
+		}
+	}
+
+}
---
+++ pig-common/pig-common-seata/src/main/java/com/pig4cloud/pig/common/seata/config/SeataAutoConfiguration.java
@@ -0,0 +1,19 @@
+package com.pig4cloud.pig.common.seata.config;
+
+import com.pig4cloud.pig.common.core.factory.YamlPropertySourceFactory;
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Seata 配置类
+ *
+ * @author lengleng
+ * @date 2022/3/29
+ */
+@PropertySource(value = "classpath:seata-config.yml", factory = YamlPropertySourceFactory.class)
+@EnableAutoDataSourceProxy
+@Configuration(proxyBeanMethods = false)
+public class SeataAutoConfiguration {
+
+}
