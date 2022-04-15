7a963542cd2bb3e1fefb9289f80c5a0519691e84
付绪壮 <2235602974@qq.com>
2022-04-15 14:30:23 +0800

    :arrow_up: up sentinel 1.8.4

--- pigx-visual/pigx-sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/FlowControllerV2.java
+++ pigx-visual/pigx-sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/controller/v2/FlowControllerV2.java
@@ -16,7 +16,6 @@
 package com.alibaba.csp.sentinel.dashboard.controller.v2;
 
 import com.alibaba.csp.sentinel.dashboard.auth.AuthAction;
-import com.alibaba.csp.sentinel.dashboard.auth.AuthService;
 import com.alibaba.csp.sentinel.dashboard.auth.AuthService.PrivilegeType;
 import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
 import com.alibaba.csp.sentinel.dashboard.domain.Result;
@@ -127,7 +126,7 @@ public class FlowControllerV2 {
 	}
 
 	@PostMapping("/rule")
-	@AuthAction(value = AuthService.PrivilegeType.WRITE_RULE)
+	@AuthAction(value = PrivilegeType.WRITE_RULE)
 	public Result<FlowRuleEntity> apiAddFlowRule(@RequestBody FlowRuleEntity entity) {
 
 		Result<FlowRuleEntity> checkResult = checkEntityInternal(entity);
@@ -152,7 +151,7 @@ public class FlowControllerV2 {
 	}
 
 	@PutMapping("/rule/{id}")
-	@AuthAction(AuthService.PrivilegeType.WRITE_RULE)
+	@AuthAction(PrivilegeType.WRITE_RULE)
 
 	public Result<FlowRuleEntity> apiUpdateFlowRule(@PathVariable("id") Long id, @RequestBody FlowRuleEntity entity) {
 		if (id == null || id <= 0) {
