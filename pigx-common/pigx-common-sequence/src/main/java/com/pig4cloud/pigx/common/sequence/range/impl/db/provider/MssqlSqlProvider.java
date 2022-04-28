c41c0c7a5291281c3a9fb63a289e9433d553cf13
lbw <wangiegie@163.com>
2022-04-28 13:43:24 +0800

    :card_file_box: 执行数据库相关的变化， 支持 达梦数据库

--- pigx-common/pigx-common-sequence/src/main/java/com/pig4cloud/pigx/common/sequence/range/impl/db/provider/MssqlSqlProvider.java
+++ pigx-common/pigx-common-sequence/src/main/java/com/pig4cloud/pigx/common/sequence/range/impl/db/provider/MssqlSqlProvider.java
@@ -15,27 +15,20 @@ import org.springframework.stereotype.Component;
 public class MssqlSqlProvider implements SqlProvider {
 
 	/**
-	 * 获取表是否存在
-	 * @return 执行语句
-	 */
-	@Override
-	public String getExistTableSql() {
-		return "SELECT TOP 1 * FROM sysObjects WHERE Id=OBJECT_ID(N'#tableName') AND xtype='U'";
-	}
-
-	/**
 	 * 获取建表语句
 	 * @return
 	 */
 	@Override
 	public String getCreateTableSql() {
-		return "CREATE TABLE #tableName (id bigint NOT NULL,value bigint NOT NULL,name nvarchar (32) COLLATE "
-				+ "Chinese_PRC_CI_AS NOT NULL,gmt_create datetime2 (7) NOT NULL,gmt_modified datetime2 (7) NOT NULL)";
+		return "IF NOT EXISTS (\n"
+				+ "SELECT*FROM sys.all_objects WHERE object_id=OBJECT_ID(N'#tableName') AND type IN ('U')) \n"
+				+ "CREATE TABLE #tableName (id bigint NOT NULL,VALUE bigint NOT NULL,name nvarchar (32) COLLATE "
+				+ "Chinese_PRC_CI_AS NOT NULL,gmt_create datetime2 (7) NOT NULL,gmt_modified datetime2 (7) NOT NULL) GO";
 	}
 
 	@Override
 	public Boolean support(DbType dbType) {
-		return DbType.SQL_SERVER.equals(dbType);
+		return DbType.SQL_SERVER2005.equals(dbType);
 	}
 
 }
