900ea24491cee8e9fd4f3409ca5dbd8f4ae06730
lbw <wangiegie@163.com>
2022-07-27 14:57:11 +0800

    :art: Improving structure / format of the code. 格式化代码

--- pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/service/PigRemoteRegisteredClientRepository.java
+++ pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/service/PigRemoteRegisteredClientRepository.java
@@ -100,12 +100,12 @@ public class PigRemoteRegisteredClientRepository implements RegisteredClientRepo
 				.ifPresent(grants -> StringUtils.commaDelimitedListToSet(grants)
 						.forEach(s -> builder.authorizationGrantType(new AuthorizationGrantType(s))));
 		// 回调地址
-		Optional.ofNullable(clientDetails.getWebServerRedirectUri()).ifPresent(redirectUri -> Arrays.stream(redirectUri.split(","))
-				.filter(StrUtil::isNotBlank).forEach(builder::redirectUri));
+		Optional.ofNullable(clientDetails.getWebServerRedirectUri()).ifPresent(redirectUri -> Arrays
+				.stream(redirectUri.split(",")).filter(StrUtil::isNotBlank).forEach(builder::redirectUri));
 
 		// scope
-		Optional.ofNullable(clientDetails.getScope()).ifPresent(scope -> Arrays.stream(scope.split(","))
-				.filter(StrUtil::isNotBlank).forEach(builder::scope));
+		Optional.ofNullable(clientDetails.getScope()).ifPresent(
+				scope -> Arrays.stream(scope.split(",")).filter(StrUtil::isNotBlank).forEach(builder::scope));
 
 		return builder
 				.tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.REFERENCE)
