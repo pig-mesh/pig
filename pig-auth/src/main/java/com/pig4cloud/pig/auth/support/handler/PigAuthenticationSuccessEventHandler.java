64ae0dddb63fc9a496c74a8bcd60c94c1f69f542
付绪壮 <2235602974@qq.com>
2022-08-12 21:29:30 +0800

    :art: 代码格式化

--- pig-auth/src/main/java/com/pig4cloud/pig/auth/support/handler/PigAuthenticationSuccessEventHandler.java
+++ pig-auth/src/main/java/com/pig4cloud/pig/auth/support/handler/PigAuthenticationSuccessEventHandler.java
@@ -52,16 +52,15 @@ public class PigAuthenticationSuccessEventHandler implements AuthenticationSucce
 
 	/**
 	 * Called when a user has been successfully authenticated.
-	 *
-	 * @param request        the request which caused the successful authentication
-	 * @param response       the response
+	 * @param request the request which caused the successful authentication
+	 * @param response the response
 	 * @param authentication the <tt>Authentication</tt> object which was created during
-	 *                       the authentication process.
+	 * the authentication process.
 	 */
 	@SneakyThrows
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-										Authentication authentication) {
+			Authentication authentication) {
 		OAuth2AccessTokenAuthenticationToken accessTokenAuthentication = (OAuth2AccessTokenAuthenticationToken) authentication;
 		Map<String, Object> map = accessTokenAuthentication.getAdditionalParameters();
 		PigUser userInfo = (PigUser) map.get("user_info");
@@ -84,7 +83,7 @@ public class PigAuthenticationSuccessEventHandler implements AuthenticationSucce
 	}
 
 	private void sendAccessTokenResponse(HttpServletRequest request, HttpServletResponse response,
-										 Authentication authentication) throws IOException {
+			Authentication authentication) throws IOException {
 
 		OAuth2AccessTokenAuthenticationToken accessTokenAuthentication = (OAuth2AccessTokenAuthenticationToken) authentication;
 
