6991cdad557f4b9d79bba50d6aeede4edfe30eb2
Lht <lht1435354159@163.com>
2023-03-30 19:02:47 +0800

    :arrow_up: Upgrading dependencies. UP xxl-job 2.4.0

--- pig-visual/pig-xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobLogController.java
+++ pig-visual/pig-xxl-job-admin/src/main/java/com/xxl/job/admin/controller/JobLogController.java
@@ -138,23 +138,29 @@ public class JobLogController {
 
 		model.addAttribute("triggerCode", jobLog.getTriggerCode());
 		model.addAttribute("handleCode", jobLog.getHandleCode());
-		model.addAttribute("executorAddress", jobLog.getExecutorAddress());
-		model.addAttribute("triggerTime", jobLog.getTriggerTime().getTime());
 		model.addAttribute("logId", jobLog.getId());
 		return "joblog/joblog.detail";
 	}
 
 	@RequestMapping("/logDetailCat")
 	@ResponseBody
-	public ReturnT<LogResult> logDetailCat(String executorAddress, long triggerTime, long logId, int fromLineNum) {
+	public ReturnT<LogResult> logDetailCat(long logId, int fromLineNum) {
 		try {
-			ExecutorBiz executorBiz = XxlJobScheduler.getExecutorBiz(executorAddress);
-			ReturnT<LogResult> logResult = executorBiz.log(new LogParam(triggerTime, logId, fromLineNum));
+			// valid
+			XxlJobLog jobLog = xxlJobLogDao.load(logId); // todo, need to improve
+															// performance
+			if (jobLog == null) {
+				return new ReturnT<LogResult>(ReturnT.FAIL_CODE, I18nUtil.getString("joblog_logid_unvalid"));
+			}
+
+			// log cat
+			ExecutorBiz executorBiz = XxlJobScheduler.getExecutorBiz(jobLog.getExecutorAddress());
+			ReturnT<LogResult> logResult = executorBiz
+				.log(new LogParam(jobLog.getTriggerTime().getTime(), logId, fromLineNum));
 
 			// is end
 			if (logResult.getContent() != null
 					&& logResult.getContent().getFromLineNum() > logResult.getContent().getToLineNum()) {
-				XxlJobLog jobLog = xxlJobLogDao.load(logId);
 				if (jobLog.getHandleCode() > 0) {
 					logResult.getContent().setEnd(true);
 				}
