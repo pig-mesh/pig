8f026ada0f89f8fc33fdd2f20eacf21eb6a8ada4
lbw <wangiegie@163.com>
2023-04-27 14:56:39 +0800

    :art: Improving structure / format of the code. 格式化代码

--- pig-upms/pig-upms-biz/src/main/java/com/pig4cloud/pig/admin/controller/SysDictController.java
+++ pig-upms/pig-upms-biz/src/main/java/com/pig4cloud/pig/admin/controller/SysDictController.java
@@ -75,9 +75,10 @@ public class SysDictController {
 	 */
 	@GetMapping("/page")
 	public R<IPage<SysDict>> getDictPage(Page page, SysDict sysDict) {
-		return R.ok(sysDictService.page(page, Wrappers.<SysDict>lambdaQuery()
-			.like(StrUtil.isNotBlank(sysDict.getDictKey()), SysDict::getDictKey, sysDict.getDictKey())
-				.orderByDesc(SysDict::getUpdateTime)));
+		return R.ok(sysDictService.page(page,
+				Wrappers.<SysDict>lambdaQuery()
+					.like(StrUtil.isNotBlank(sysDict.getDictKey()), SysDict::getDictKey, sysDict.getDictKey())
+					.orderByDesc(SysDict::getUpdateTime)));
 	}
 
 	/**
@@ -88,7 +89,10 @@ public class SysDictController {
 	@GetMapping("/key/{key}")
 	@Cacheable(value = CacheConstants.DICT_DETAILS, key = "#key")
 	public R<List<SysDictItem>> getDictByKey(@PathVariable String key) {
-		return R.ok(sysDictItemService.list(Wrappers.<SysDictItem>query().lambda().eq(SysDictItem::getDictKey, key).orderByAsc(SysDictItem::getSortOrder)));
+		return R.ok(sysDictItemService.list(Wrappers.<SysDictItem>query()
+			.lambda()
+			.eq(SysDictItem::getDictKey, key)
+			.orderByAsc(SysDictItem::getSortOrder)));
 	}
 
 	/**
