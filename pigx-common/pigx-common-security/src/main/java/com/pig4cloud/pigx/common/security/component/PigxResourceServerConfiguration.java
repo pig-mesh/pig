43da5a1dbf5e80c856d08d57028caee176be88cd
lbw <wangiegie@163.com>
2023-07-15 16:31:38 +0800

    :recycle: Refactoring code. 避免SAS 权限拦截失效

--- pigx-common/pigx-common-security/src/main/java/com/pig4cloud/pigx/common/security/component/PigxResourceServerConfiguration.java
+++ pigx-common/pigx-common-security/src/main/java/com/pig4cloud/pigx/common/security/component/PigxResourceServerConfiguration.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -38,7 +38,7 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Slf4j
 @EnableWebSecurity
-@EnableGlobalMethodSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class PigxResourceServerConfiguration {
 
