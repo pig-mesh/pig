dc24f0fc4b754534d873bcc424765deac88cfb90
lbw <wangiegie@163.com>
2023-08-15 21:33:49 +0800

    :arrow_up: Upgrading dependencies. up nacos 2.2.4 close #I7THAS

--- pig-register/src/main/java/com/alibaba/nacos/config/ConsoleConfig.java
+++ pig-register/src/main/java/com/alibaba/nacos/config/ConsoleConfig.java
@@ -39,7 +39,7 @@ import java.time.ZoneId;
  */
 @Component
 @EnableScheduling
-@PropertySource("/application.properties")
+@PropertySource("/application.yml")
 public class ConsoleConfig {
 
 	@Autowired
--- pig-register/src/main/java/com/alibaba/nacos/console/model/form/NamespaceForm.java
+++ pig-register/src/main/java/com/alibaba/nacos/console/model/form/NamespaceForm.java
@@ -26,90 +26,86 @@ import java.util.Objects;
 
 /**
  * NamespaceForm.
- *
  * @author dongyafei
  * @date 2022/8/16
  */
 public class NamespaceForm implements Serializable {
-
-	private static final long serialVersionUID = -1078976569495343487L;
-
-	private String namespaceId;
-
-	private String namespaceName;
-
-	private String namespaceDesc;
-
-	public NamespaceForm() {
-	}
-
-	public NamespaceForm(String namespaceId, String namespaceName, String namespaceDesc) {
-		this.namespaceId = namespaceId;
-		this.namespaceName = namespaceName;
-		this.namespaceDesc = namespaceDesc;
-	}
-
-	public String getNamespaceId() {
-		return namespaceId;
-	}
-
-	public void setNamespaceId(String namespaceId) {
-		this.namespaceId = namespaceId;
-	}
-
-	public String getNamespaceName() {
-		return namespaceName;
-	}
-
-	public void setNamespaceName(String namespaceName) {
-		this.namespaceName = namespaceName;
-	}
-
-	public String getNamespaceDesc() {
-		return namespaceDesc;
-	}
-
-	public void setNamespaceDesc(String namespaceDesc) {
-		this.namespaceDesc = namespaceDesc;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		NamespaceForm that = (NamespaceForm) o;
-		return Objects.equals(namespaceId, that.namespaceId) && Objects.equals(namespaceName, that.namespaceName)
-				&& Objects.equals(namespaceDesc, that.namespaceDesc);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(namespaceId, namespaceName, namespaceDesc);
-	}
-
-	@Override
-	public String toString() {
-		return "NamespaceVo{" + "namespaceId='" + namespaceId + '\'' + ", namespaceName='" + namespaceName + '\''
-				+ ", namespaceDesc='" + namespaceDesc + '\'' + '}';
-	}
-
-	/**
-	 * check required param.
-	 * @throws NacosException NacosException
-	 */
-	public void validate() throws NacosException {
-		if (null == namespaceId) {
-			throw new NacosApiException(HttpStatus.BAD_REQUEST.value(), ErrorCode.PARAMETER_MISSING,
-					"required parameter 'namespaceId' is missing");
-		}
-		if (null == namespaceName) {
-			throw new NacosApiException(HttpStatus.BAD_REQUEST.value(), ErrorCode.PARAMETER_MISSING,
-					"required parameter 'namespaceName' is missing");
-		}
-	}
-
+    
+    private static final long serialVersionUID = -1078976569495343487L;
+    
+    private String namespaceId;
+    
+    private String namespaceName;
+    
+    private String namespaceDesc;
+    
+    public NamespaceForm() {
+    }
+    
+    public NamespaceForm(String namespaceId, String namespaceName, String namespaceDesc) {
+        this.namespaceId = namespaceId;
+        this.namespaceName = namespaceName;
+        this.namespaceDesc = namespaceDesc;
+    }
+    
+    public String getNamespaceId() {
+        return namespaceId;
+    }
+    
+    public void setNamespaceId(String namespaceId) {
+        this.namespaceId = namespaceId;
+    }
+    
+    public String getNamespaceName() {
+        return namespaceName;
+    }
+    
+    public void setNamespaceName(String namespaceName) {
+        this.namespaceName = namespaceName;
+    }
+    
+    public String getNamespaceDesc() {
+        return namespaceDesc;
+    }
+    
+    public void setNamespaceDesc(String namespaceDesc) {
+        this.namespaceDesc = namespaceDesc;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NamespaceForm that = (NamespaceForm) o;
+        return Objects.equals(namespaceId, that.namespaceId) && Objects.equals(namespaceName, that.namespaceName)
+                && Objects.equals(namespaceDesc, that.namespaceDesc);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(namespaceId, namespaceName, namespaceDesc);
+    }
+    
+    @Override
+    public String toString() {
+        return "NamespaceVo{" + "namespaceId='" + namespaceId + '\'' + ", namespaceName='" + namespaceName + '\''
+                + ", namespaceDesc='" + namespaceDesc + '\'' + '}';
+    }
+    
+    /**
+     * check required param.
+     * @throws NacosException NacosException
+     */
+    public void validate() throws NacosException {
+        if (null == namespaceId) {
+            throw new NacosApiException(HttpStatus.BAD_REQUEST.value(), ErrorCode.PARAMETER_MISSING, "required parameter 'namespaceId' is missing");
+        }
+        if (null == namespaceName) {
+            throw new NacosApiException(HttpStatus.BAD_REQUEST.value(), ErrorCode.PARAMETER_MISSING, "required parameter 'namespaceName' is missing");
+        }
+    }
 }
