80adb67cff3a5040318560f2e8736a7cea5ef8ac
lbw <wangiegie@163.com>
2023-08-17 21:46:22 +0800

    :bookmark: Releasing / Version tags. 3.7.0-JDK8

--- pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/component/PigSecurityInnerAspect.java
+++ pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/component/PigSecurityInnerAspect.java
@@ -16,8 +16,10 @@
 
 package com.pig4cloud.pig.common.security.component;
 
+import cn.hutool.core.util.StrUtil;
 import com.pig4cloud.pig.common.core.constant.SecurityConstants;
 import com.pig4cloud.pig.common.security.annotation.Inner;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -25,9 +27,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.security.access.AccessDeniedException;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author lengleng
@@ -46,13 +47,12 @@ public class PigSecurityInnerAspect implements Ordered {
 	@Before("@within(inner) || @annotation(inner)")
 	public void around(JoinPoint point, Inner inner) {
 		// 实际注入的inner实体由表达式后一个注解决定，即是方法上的@Inner注解实体，若方法上无@Inner注解，则获取类上的
-		// 这段代码没有意义，拦截的就是@Inner注解，怎么会为null呢
-		// if (inner == null) {
-		// Class<?> clazz = point.getTarget().getClass();
-		// inner = AnnotationUtils.findAnnotation(clazz, Inner.class);
-		// }
+		if (inner == null) {
+			Class<?> clazz = point.getTarget().getClass();
+			inner = AnnotationUtils.findAnnotation(clazz, Inner.class);
+		}
 		String header = request.getHeader(SecurityConstants.FROM);
-		if (inner.value() && !SecurityConstants.FROM_IN.equals(header)) {
+		if (inner.value() && !StrUtil.equals(SecurityConstants.FROM_IN, header)) {
 			log.warn("访问接口 {} 没有权限", point.getSignature().getName());
 			throw new AccessDeniedException("Access is denied");
 		}
