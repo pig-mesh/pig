22bc9ef90eb6ecf6ef867c3a1e4f89cd2440dfe3
lbw <wangiegie@163.com>
2023-10-27 17:38:47 +0800

    :lock: Fixing security issues. 避免C端用户携带token 访问 B端接口

---
+++ pigx-common/pigx-common-core/src/main/java/com/pig4cloud/pigx/common/core/constant/enums/YesNoEnum.java
@@ -0,0 +1,28 @@
+package com.pig4cloud.pigx.common.core.constant.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * yes and no enum
+ *
+ * @author lengleng
+ * @date 2023/10/25
+ */
+@Getter
+@AllArgsConstructor
+public enum YesNoEnum {
+
+    YES("1", "是"),
+    NO("0", "否");
+
+    /**
+     * 编码
+     */
+    private String code;
+
+    /**
+     * 描述
+     */
+    private String desc;
+}
