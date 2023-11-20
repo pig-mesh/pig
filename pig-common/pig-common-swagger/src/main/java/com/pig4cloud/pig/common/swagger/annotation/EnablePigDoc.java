57d6d9c1c9425181ed2d883a4149e8840ad66b27
lbw <wangiegie@163.com>
2023-11-20 15:07:39 +0800

    :recycle: Refactoring code. 重构 swagger 支持 yaml 通用配置

--- pig-common/pig-common-swagger/src/main/java/com/pig4cloud/pig/common/swagger/annotation/EnablePigDoc.java
+++ pig-common/pig-common-swagger/src/main/java/com/pig4cloud/pig/common/swagger/annotation/EnablePigDoc.java
@@ -16,10 +16,12 @@
 
 package com.pig4cloud.pig.common.swagger.annotation;
 
+import com.pig4cloud.pig.common.core.factory.YamlPropertySourceFactory;
 import com.pig4cloud.pig.common.swagger.config.OpenAPIDefinitionImportSelector;
 import com.pig4cloud.pig.common.swagger.support.SwaggerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 import java.lang.annotation.*;
 
@@ -29,24 +31,27 @@ import java.lang.annotation.*;
  * @author lengleng
  * @date 2022-03-26
  */
-@Target({ ElementType.TYPE })
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
 @EnableConfigurationProperties(SwaggerProperties.class)
 @Import(OpenAPIDefinitionImportSelector.class)
+@PropertySource(value = "classpath:openapi-config.yaml", factory = YamlPropertySourceFactory.class)
 public @interface EnablePigDoc {
 
-	/**
-	 * 网关路由前缀
-	 * @return String
-	 */
-	String value();
+    /**
+     * 网关路由前缀
+     *
+     * @return String
+     */
+    String value();
 
-	/**
-	 * 是否是微服务架构
-	 * @return true
-	 */
-	boolean isMicro() default false;
+    /**
+     * 是否是微服务架构
+     *
+     * @return true
+     */
+    boolean isMicro() default false;
 
 }
