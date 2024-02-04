d7f9cb21fbb3d19f7e136c8ef4547f3bdc7caa33
luolin <766488893@qq.com>
2024-02-04 17:43:55 +0800

    :recycle: Refactoring code.新增角色与菜单VO对象

--- pigx-upms/pigx-upms-api/src/main/java/com/pig4cloud/pigx/admin/api/vo/RoleMenuVO.java
+++ pigx-upms/pigx-upms-api/src/main/java/com/pig4cloud/pigx/admin/api/vo/RoleMenuVO.java
@@ -20,75 +20,22 @@ package com.pig4cloud.pigx.admin.api.vo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
 /**
- * <p>
- * 角色表
- * </p>
- *
  * @author lengleng
  * @date 2020/2/10
  */
 @Data
 @Schema(description = "前端角色展示对象")
-public class RoleVO implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+public class RoleMenuVO {
 
 	/**
-	 * 角色ID
+	 * 角色id
 	 */
-	@Schema(description = "角色ID")
 	private Long roleId;
 
 	/**
-	 * 角色名称
+	 * 菜单列表
 	 */
-	@Schema(description = "角色名称")
-	private String roleName;
-
-	/**
-	 * 角色标识
-	 */
-	@Schema(description = "角色标识")
-	private String roleCode;
-
-	/**
-	 * 角色描述
-	 */
-	@Schema(description = "角色描述")
-	private String roleDesc;
-
-	/**
-	 * 数据权限类型
-	 */
-	@Schema(description = "数据权限类型")
-	private Integer dsType;
-
-	/**
-	 * 数据权限作用范围
-	 */
-	@Schema(description = "数据权限作用范围")
-	private String dsScope;
-
-	/**
-	 * 创建时间
-	 */
-	@Schema(description = "创建时间")
-	private LocalDateTime createTime;
-
-	/**
-	 * 修改时间
-	 */
-	@Schema(description = "修改时间")
-	private LocalDateTime updateTime;
-
-	/**
-	 * 租户ID
-	 */
-	@Schema(description = "用户所属租户id")
-	private Long tenantId;
+	private String menuIds;
 
 }
