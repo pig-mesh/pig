c515d285e918ecda2e6c854b100958abda807041
冷冷 <2270033969@qq.com>
2024-03-15 21:55:38 +0800

    ♻️ add feign header connection=close fix RetryableException #I97DZH

---
+++ pig-common/pig-common-feign/src/main/java/com/pig4cloud/pig/common/feign/core/PigFeignRequestCloseInterceptor.java
@@ -0,0 +1,23 @@
+package com.pig4cloud.pig.common.feign.core;
+
+import feign.RequestInterceptor;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * @author lengleng
+ * @date 2024/3/15
+ * <p>
+ * http connection close
+ */
+public class PigFeignRequestCloseInterceptor implements RequestInterceptor {
+
+    /**
+     * set connection close
+     *
+     * @param template
+     */
+    @Override
+    public void apply(feign.RequestTemplate template) {
+        template.header(HttpHeaders.CONNECTION, "close");
+    }
+}
