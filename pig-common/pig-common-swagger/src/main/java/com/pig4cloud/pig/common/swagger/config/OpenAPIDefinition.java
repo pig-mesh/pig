4b03d61d184be847046fe51be03795ad4a04b3f1
冷冷 <2270033969@qq.com>
2024-04-04 21:10:50 +0800

    :recycle: Refactoring code. 重构 common-swagger 模块

--- pig-common/pig-common-swagger/src/main/java/com/pig4cloud/pig/common/swagger/config/OpenAPIDefinition.java
+++ pig-common/pig-common-swagger/src/main/java/com/pig4cloud/pig/common/swagger/config/OpenAPIDefinition.java
@@ -42,9 +42,9 @@ import java.util.List;
  *
  * <p>
  * 禁用方法1：使用注解@Profile({"dev","test"})
- *
+ * <p>
  * 表示在开发或测试环境开启，而在生产关闭。（推荐使用） 禁用方法2：使用注解@ConditionalOnProperty(name = "swagger.enable",
- *
+ * <p>
  * havingValue = "true") 然后在测试配置或者开发配置中添加swagger.enable=true即可开启，生产环境不填则默认关闭Swagger.
  * </p>
  *
@@ -72,7 +72,7 @@ public class OpenAPIDefinition extends OpenAPI implements InitializingBean, Appl
 	}
 
 	@Override
-	public void afterPropertiesSet() {
+	public void afterPropertiesSet() throws Exception {
 		SwaggerProperties swaggerProperties = applicationContext.getBean(SwaggerProperties.class);
 		this.info(new Info().title(swaggerProperties.getTitle()));
 		// oauth2.0 password
