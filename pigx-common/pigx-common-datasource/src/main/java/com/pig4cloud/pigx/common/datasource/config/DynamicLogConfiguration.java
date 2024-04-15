6ba8ce326ddba40e76d03d5ab49c110ee14cb505
冷冷 <2270033969@qq.com>
2024-04-15 22:54:37 +0800

    :mute: 删除Dynamic-ds-log 配置

--- pigx-common/pigx-common-datasource/src/main/java/com/pig4cloud/pigx/common/datasource/config/DynamicLogConfiguration.java
+++ pigx-common/pigx-common-datasource/src/main/java/com/pig4cloud/pigx/common/datasource/config/DynamicLogConfiguration.java
@@ -1,8 +1,6 @@
 package com.pig4cloud.pigx.common.datasource.config;
 
-import com.pig4cloud.pigx.common.core.factory.YamlPropertySourceFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author lengleng
@@ -11,7 +9,6 @@ import org.springframework.context.annotation.PropertySource;
  * 注入SQL 格式化的插件
  */
 @ConditionalOnClass(name = "com.pig4cloud.pigx.common.data.mybatis.DruidSqlLogFilter")
-@PropertySource(value = "classpath:dynamic-ds-log.yaml", factory = YamlPropertySourceFactory.class)
 public class DynamicLogConfiguration {
 
 }
