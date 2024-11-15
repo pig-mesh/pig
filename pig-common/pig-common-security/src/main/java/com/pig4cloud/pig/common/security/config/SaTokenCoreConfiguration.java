ef4efe9d1cd038f8dd99361698bd8a0d0c13a526
冷冷 <2270033969@qq.com>
2024-11-15 15:00:15 +0800

    refactor(sa-token): 关闭 banner 打印，异常复用 spring security的异常I18N文件

--- pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/config/SaTokenCoreConfiguration.java
+++ pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/config/SaTokenCoreConfiguration.java
@@ -5,7 +5,10 @@ import cn.dev33.satoken.config.SaTokenConfig;
 import com.pig4cloud.pig.common.core.constant.SecurityConstants;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 /**
  * SA Token 核心配置
@@ -21,11 +24,28 @@ public class SaTokenCoreConfiguration implements InitializingBean {
 		cfg.setTokenName(SecurityConstants.PROJECT_PREFIX);
 	}
 
+	/**
+	 * 安全异常消息源
+	 *
+	 * @return {@link MessageSource }
+	 */
+	@Bean
+	public MessageSource securityMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/errors/messages");
+		return messageSource;
+	}
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		// 允许同时在线
 		SaManager.getConfig().setIsConcurrent(true);
+		// 不允许复用 token
 		SaManager.getConfig().setIsShare(false);
+		// 关闭打印日志
+		SaManager.getConfig().setIsPrint(false);
 	}
+
+
 
 }
