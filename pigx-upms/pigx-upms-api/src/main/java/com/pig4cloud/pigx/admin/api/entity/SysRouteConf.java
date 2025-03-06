a233aa5b5abd1fde85e5ba9ade17fe7b082051d9
冷冷 <2270033969@qq.com>
2025-03-06 13:03:42 +0800

    refactor(upms-api): 优化 `SysRouteConf` 实体类的字段填充逻辑
    在 `SysRouteConf` 实体类中，为 `createBy`、`updateBy`、`createTime`、`updateTime` 和 `delFlag` 字段添加了 `@TableField` 注解，以自动填充

--- pigx-upms/pigx-upms-api/src/main/java/com/pig4cloud/pigx/admin/api/entity/SysRouteConf.java
+++ pigx-upms/pigx-upms-api/src/main/java/com/pig4cloud/pigx/admin/api/entity/SysRouteConf.java
@@ -17,9 +17,7 @@
 
 package com.pig4cloud.pigx.admin.api.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -39,69 +37,86 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode(callSuper = true)
 public class SysRouteConf extends Model<SysRouteConf> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@JsonIgnore
-	@TableId(type = IdType.ASSIGN_ID)
-	@Schema(description = "主键")
-	private Long id;
+    @JsonIgnore
+    @TableId(type = IdType.ASSIGN_ID)
+    @Schema(description = "主键")
+    private Long id;
 
-	/**
-	 * 路由ID
-	 */
-	@Schema(description = "路由id")
-	private String routeId;
+    /**
+     * 路由ID
+     */
+    @Schema(description = "路由id")
+    private String routeId;
 
-	/**
-	 * 路由名称
-	 */
-	@Schema(description = "路由名称")
-	private String routeName;
+    /**
+     * 路由名称
+     */
+    @Schema(description = "路由名称")
+    private String routeName;
 
-	/**
-	 * 断言
-	 */
-	@Schema(description = "断言")
-	private String predicates;
+    /**
+     * 断言
+     */
+    @Schema(description = "断言")
+    private String predicates;
 
-	/**
-	 * 过滤器
-	 */
-	@Schema(description = "过滤器")
-	private String filters;
+    /**
+     * 过滤器
+     */
+    @Schema(description = "过滤器")
+    private String filters;
 
-	/**
-	 * uri
-	 */
-	@Schema(description = "请求uri")
-	private String uri;
+    /**
+     * uri
+     */
+    @Schema(description = "请求uri")
+    private String uri;
 
-	/**
-	 * 排序
-	 */
-	@Schema(description = "排序值")
-	private Integer sortOrder;
+    /**
+     * 排序
+     */
+    @Schema(description = "排序值")
+    private Integer sortOrder;
 
-	@Schema(description = "元数据")
-	private String metadata;
+    @Schema(description = "元数据")
+    private String metadata;
 
-	/**
-	 * 创建时间
-	 */
-	@Schema(description = "创建时间")
-	private LocalDateTime createTime;
+    /**
+     * 创建人
+     */
+    @TableField(fill = FieldFill.INSERT)
+    @Schema(description = "创建人")
+    private String createBy;
 
-	/**
-	 * 修改时间
-	 */
-	@Schema(description = "修改时间")
-	private LocalDateTime updateTime;
+    /**
+     * 修改人
+     */
+    @TableField(fill = FieldFill.UPDATE)
+    @Schema(description = "修改人")
+    private String updateBy;
 
-	/**
-	 * 删除标识（0-正常,1-删除）
-	 */
-	@TableLogic
-	@Schema(description = "删除标记,1:已删除,0:正常")
-	private String delFlag;
+    /**
+     * 创建时间
+     */
+    @Schema(description = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @Schema(description = "修改时间")
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
+
+    /**
+     * 删除标识（0-正常,1-删除）
+     */
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
+    @Schema(description = "删除标记,1:已删除,0:正常")
+    private String delFlag;
 
 }
