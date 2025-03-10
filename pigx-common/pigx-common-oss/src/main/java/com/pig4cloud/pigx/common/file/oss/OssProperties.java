9f7f4c45641335e5909e8002feaad035aa222a36
冷冷 <2270033969@qq.com>
2025-03-10 13:01:53 +0800

    feat(oss): 添加 `skipMd5Check` 配置项以支持跳过 MD5 检查
    #6272 使用腾讯云存储COS ，图片不能正常下载的问题， Invalid base 16 character: '"'

--- pigx-common/pigx-common-oss/src/main/java/com/pig4cloud/pigx/common/file/oss/OssProperties.java
+++ pigx-common/pigx-common-oss/src/main/java/com/pig4cloud/pigx/common/file/oss/OssProperties.java
@@ -35,46 +35,51 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "oss")
 public class OssProperties {
 
-	/**
-	 * 对象存储服务的URL
-	 */
-	private String endpoint;
+    /**
+     * 对象存储服务的URL
+     */
+    private String endpoint;
 
-	/**
-	 * 自定义域名
-	 */
-	private String customDomain;
+    /**
+     * 自定义域名
+     */
+    private String customDomain;
 
-	/**
-	 * true path-style nginx 反向代理和S3默认支持 pathStyle {http://endpoint/bucketname} false
-	 * supports virtual-hosted-style 阿里云等需要配置为 virtual-hosted-style
-	 * 模式{http://bucketname.endpoint}
-	 */
-	private Boolean pathStyleAccess = true;
+    /**
+     * true path-style nginx 反向代理和S3默认支持 pathStyle {http://endpoint/bucketname} false
+     * supports virtual-hosted-style 阿里云等需要配置为 virtual-hosted-style
+     * 模式{http://bucketname.endpoint}
+     */
+    private Boolean pathStyleAccess = true;
 
-	/**
-	 * 应用ID
-	 */
-	private String appId;
+    /**
+     * 应用ID
+     */
+    private String appId;
 
-	/**
-	 * 区域
-	 */
-	private String region;
+    /**
+     * 区域
+     */
+    private String region;
 
-	/**
-	 * Access key就像用户ID，可以唯一标识你的账户
-	 */
-	private String accessKey;
+    /**
+     * Access key就像用户ID，可以唯一标识你的账户
+     */
+    private String accessKey;
 
-	/**
-	 * Secret key是你账户的密码
-	 */
-	private String secretKey;
+    /**
+     * Secret key是你账户的密码
+     */
+    private String secretKey;
 
-	/**
-	 * 最大线程数，默认： 100
-	 */
-	private Integer maxConnections = 100;
+    /**
+     * 最大线程数，默认： 100
+     */
+    private Integer maxConnections = 100;
+
+    /**
+     * 跳过 MD5 检查
+     */
+    private boolean skipMd5Check;
 
 }
