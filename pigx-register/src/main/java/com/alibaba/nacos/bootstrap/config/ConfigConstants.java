d87172db050de207264b86f7232ec89cba85d033
冷冷 <wangiegie@gmail.com>
2025-05-01 19:31:17 +0800

    feat(register): 更新Nacos版本至3.0.0， 请注意端口是 18080

--- pigx-register/src/main/java/com/alibaba/nacos/bootstrap/config/ConfigConstants.java
+++ pigx-register/src/main/java/com/alibaba/nacos/bootstrap/config/ConfigConstants.java
@@ -15,7 +15,7 @@
  * Author: lengleng (wangiegie@gmail.com)
  */
 
-package com.alibaba.nacos.console.config;
+package com.alibaba.nacos.bootstrap.config;
 
 /**
  * @author lengleng
@@ -26,28 +26,13 @@ package com.alibaba.nacos.console.config;
 public interface ConfigConstants {
 
 	/**
-	 * The System property name of Standalone mode
+	 * 独立模式系统属性名称
 	 */
 	String STANDALONE_MODE = "nacos.standalone";
 
 	/**
-	 * 是否开启认证
+	 * Nacos控制台端口号配置键
 	 */
-	String AUTH_ENABLED = "nacos.core.auth.enabled";
-
-	/**
-	 * 日志目录
-	 */
-	String LOG_BASEDIR = "server.tomcat.basedir";
-
-	/**
-	 * access_log日志开关
-	 */
-	String LOG_ENABLED = "server.tomcat.accesslog.enabled";
-
-	/**
-	 * 路径 nacos context path
-	 */
-	String NACOS_CONTEXT_PATH = "server.servlet.contextPath";
+	String NACOS_CONSOLE_PORT = "nacos.console.port";
 
 }
