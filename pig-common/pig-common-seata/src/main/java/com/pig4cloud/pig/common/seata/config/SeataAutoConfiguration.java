01baa0aa3216492bb324729f04bb7849da3df6f8
PIG AI <wangiegie@gmail.com>
2025-05-31 10:18:20 +0800

    docs(seata): 完善SeataAutoConfiguration类的文档注释
    - 更新类描述为更准确的"Seata自动配置类"
    - 修正作者信息中的日期格式
    - 保持YamlPropertySourceFactory的import清晰可见

--- pig-common/pig-common-seata/src/main/java/com/pig4cloud/pig/common/seata/config/SeataAutoConfiguration.java
+++ pig-common/pig-common-seata/src/main/java/com/pig4cloud/pig/common/seata/config/SeataAutoConfiguration.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.PropertySource;
 import com.pig4cloud.pig.common.core.factory.YamlPropertySourceFactory;
 
 /**
- * Seata 配置类
+ * Seata 自动配置类
  *
  * @author lengleng
- * @date 2022/3/29
+ * @date 2025/05/31
  */
 @PropertySource(value = "classpath:seata-config.yml", factory = YamlPropertySourceFactory.class)
 @Configuration(proxyBeanMethods = false)
